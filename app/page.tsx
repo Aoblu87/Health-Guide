4ca60a3d702b22f6b2f76a0e3774067be5047381
Stefania <126720391+Aoblu87@users.noreply.github.com>
2024-01-19 20:20:23 +0100

    sistamato tiggle sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import { Hero } from "@/components/hero/hero";
 
 export default function Home() {
   return (
-    <div className="relative h-screen w-full lg:ps-64">
+    <main className="h-full flex items-center justify-center bg-[radial-radient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
       <Hero />
-    </div>
+    </main>
   );
 }
